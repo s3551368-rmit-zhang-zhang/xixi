d344cc3890c7b542abb6cf7f59c827092ec49397
Zhang Zhang <s3551368@w8417896.cs.rmit.edu.au>
2017-08-18 17:53:54 +1000

    Initial Commit

---
+++ Focus timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Focus timer
+//
+//  Created by Zhang Zhang on 8/18/17.
+//  Copyright © 2017 Zhang Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
