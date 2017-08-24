aa76ad53ca7c8477970cd645a9c8cfb98e4cd765
Zhang Zhang <s3551368@w8417896.cs.rmit.edu.au>
2017-08-24 20:07:49 +1000

    double kill

--- Focus timer/ViewController.swift
+++ Focus timer/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Focus timer
 //
-//  Created by Zhang Zhang on 8/18/17.
+//  Created by Zhang Zhang on 8/24/17.
 //  Copyright © 2017 Zhang Zhang. All rights reserved.
 //
 
@@ -10,16 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+   
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        sideMenus()
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     
-}
+    
+    func sideMenus()
+    {
+        if revealViewController() != nil
+        {
+            menuButton.target = revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            revealViewController().rearViewRevealWidth = 275
+            revealViewController().rightViewRevealWidth = 160
+            
+            
+            shareButton.target = revealViewController()
+            shareButton.action = #selector(SWRevealViewController.rightRevealToggle(_:))
+            
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
 
+
+}
