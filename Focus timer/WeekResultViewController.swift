0b81b0212acb111ba0c45f42bdf125b70ba049af
Zhang Zhang <s3551368@w8417896.cs.rmit.edu.au>
2017-08-26 21:16:23 +1000

    add line chart

--- Focus timer/WeekResultViewController.swift
+++ Focus timer/WeekResultViewController.swift
@@ -7,11 +7,20 @@
 //
 
 import UIKit
-
+import Charts
 class WeekResultViewController: UIViewController{
    
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    
+    @IBOutlet weak var lineChart: LineChartView!
+    
+    var numbers : [Double] = [30,60,120,25,90,50,75]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateGraph()
+        sideMenus()
         // Do any additional setup after loading the view.
     }
 
@@ -20,7 +29,46 @@ class WeekResultViewController: UIViewController{
         // Dispose of any resources that can be recreated.
     }
     
-
+    func updateGraph(){
+        var lineChartEntry  = [ChartDataEntry]() //this is the Array that will eventually be displayed on the graph.
+        
+        
+        
+        //here is the for loop
+        for i in 0..<numbers.count {
+            
+            let value = ChartDataEntry(x: Double(i), y: numbers[i]) // here we set the X and Y status in a data chart entry
+            
+            lineChartEntry.append(value) // here we add it to the data set
+        }
+        
+        let line1 = LineChartDataSet(values: lineChartEntry, label: "Mintues") //Here we convert lineChartEntry to a LineChartDataSet
+        
+        line1.colors = [NSUIColor.blue] //Sets the colour to blue
+        
+        
+        let data = LineChartData() //This is the object that will be added to the chart
+        
+        data.addDataSet(line1) //Adds the line to the dataSet
+        
+        lineChart.data = data
+    }
+    
+    func sideMenus()
+    {
+        if revealViewController() != nil
+        {
+            
+            revealViewController().rearViewRevealWidth = 180
+            revealViewController().rightViewRevealWidth = 180
+            
+            
+            shareButton.target = revealViewController()
+            shareButton.action = #selector(SWRevealViewController.rightRevealToggle(_:))
+            
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
     /*
     // MARK: - Navigation
 
