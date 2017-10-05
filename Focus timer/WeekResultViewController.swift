9336600ca15e364d1ce00c777a1736c6b3683f3a
James zhang <s3551368@student.rmit.edu.au>
2017-10-05 19:29:23 +1100

    chart

--- Focus timer/WeekResultViewController.swift
+++ Focus timer/WeekResultViewController.swift
@@ -8,18 +8,24 @@
 
 import UIKit
 import Charts
+import CoreData
 class WeekResultViewController: UIViewController{
    
     @IBOutlet weak var shareButton: UIBarButtonItem!
     
     @IBOutlet weak var lineChart: LineChartView!
     
+    let mContext = (UIApplication.shared.delegate as! AppDelegate).managedObjectContext
 
-    var numbers : [Double] = [30,60,120,25,90,50,75]
+    var time = [Int32]()
+    
+    var number : [Double] = []
+    
+    var loginedCustomer : Customer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        load()
         updateGraph()
         sideMenus()
         // Do any additional setup after loading the view.
@@ -29,10 +35,45 @@ class WeekResultViewController: UIViewController{
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    func load(){
+        let request:NSFetchRequest = Customer.fetchRequest()
+        let accountPredicate = NSPredicate(format:"accountNum =%@",AccountId.accuntnum)
+        request.predicate = accountPredicate
+        request.fetchLimit = 1
+        request.fetchOffset = 0
+        
+        request.entity = NSEntityDescription.entity(forEntityName: "Customer", in: mContext)
+        
+        do {
+            let fetchedObjects:[AnyObject]? = try mContext.fetch(request)
+            
+            for c:Customer in fetchedObjects as![Customer] {
+                self.loginedCustomer = c
+            }
+        }catch {
+            fatalError("could not search：\(error)")
+        }
+        let fetchRequest : NSFetchRequest = CustomerizeEvent.fetchRequest()
+        fetchRequest.fetchLimit = 10
+        fetchRequest.fetchOffset = 0
+        
+        fetchRequest.entity = NSEntityDescription.entity(forEntityName: "CustomerizeEvent", in: mContext)
+        
+        do {
+            let fetchedObjects:[AnyObject]? = try mContext.fetch(fetchRequest)
+            for c:CustomerizeEvent in fetchedObjects as! [CustomerizeEvent]{
+                time.append(c.timeLength)
+            }
+        }catch {
+            fatalError("could not search：\(error)")
+        }
+        
+    }
+
     func updateGraph(){
         var lineChartEntry  = [ChartDataEntry]() //this is the Array that will eventually be displayed on the graph.
         
+        let numbers = time.flatMap{ Double($0) }
         
         
         //here is the for loop
